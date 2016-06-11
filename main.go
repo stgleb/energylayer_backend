e1ba92ce3a615b43b901bcf2e463c8a8e568cafa
Gleb Stepanov <gstepanov@mirantis.com>
2016-06-11 16:35:26 +0300

    Storage for Mysql finished.

--- main.go
+++ main.go
@@ -12,23 +12,142 @@ import (
 	"flag"
 )
 
+const (
+	USER = "root"
+	PASSWORD = "1234"
+	DATABASE = "energylayer"
+	PORT = "3306"
+	HOST = "localhost"
+)
+
 type Measurement struct {
 	Timestamp   int64
 	Gpio        int
 	Voltage     int
 	Power       int
 	Temperature int
+	DeviceId   int
+}
+
+type Device struct {
+	Id int
+	Uuid string
+	UserId int
+	IpAddress string
 }
 
 // Storage interface for storing and retrieving data.
 type Storage interface {
-	Save(m Measurement) error
-	Load(count int) ([]Measurement, error)
+	SaveMeasurement(m Measurement) error
+	LoadMeasurements(count int) ([]Measurement, error)
+	GetDeviceById(uuid string) (Device, error)
+	CreateDevice(uuid, ipAddress string , userId int)
 }
 
 // MySQL storage implementation.
 type MySQLStorage struct {
-	db sql.DB
+	Database sql.DB
+}
+
+func NewMySQLStorage(dbName ,user, password string) (Storage, error) {
+	host := "localhost"
+	port := "3306"
+	uri := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, port, dbName)
+	db, err  := sql.Open("mysql", uri)
+
+	if err != nil {
+		log.Printf("Error during connecting to database %s", err.Error())
+		return nil, err
+	}
+
+	return MySQLStorage{
+		db,
+	}, nil
+}
+
+func StorageFactory(storageType string) Storage {
+	switch storageType {
+	case "mysql":
+		storage, err := NewMySQLStorage(storageType, USER, PASSWORD)
+
+		if err != nil {
+			return nil
+		}
+
+		return storage
+	}
+
+	return nil
+}
+
+func (self *MySQLStorage) Save(m Measurement) {
+	tx ,err := self.Database.Begin()
+	if err != nil {
+		log.Printf("Error while opening transaction message: %s", err.Error())
+	}
+	defer tx.Rollback()
+
+	stmt, err := tx.Prepare("INSERT measurement(voltage, power, temperature, device_id)" +
+	"VALUES (?, ?, ?, ?)")
+	defer stmt.Close()
+
+	stmt.Exec(m.Voltage, m.Power, m.Temperature, m.DeviceId)
+	err = tx.Commit()
+
+	if err != nil {
+		log.Printf("Error while commiting transaction message: %s", err.Error())
+	}
+}
+
+
+func (self *MySQLStorage) GetDeviceById(uuid string) (Device, error) {
+	tx ,err := self.Database.Begin()
+
+	if err != nil {
+		log.Printf("Error while opening transaction message: %s", err.Error())
+		return nil, err
+	}
+	defer tx.Rollback()
+
+	var uuid string
+	var ipAddr string
+	var id int
+	var user_id int
+
+	row := tx.QueryRow("select id, uuid, user_id, ip_addr from users where uuid = ?", 1)
+	err = row.Scan(&id, &uuid, &user_id, &ipAddr)
+
+	if err != nil {
+		log.Printf("Error while reading data from the row %s",err.Error())
+		return nil, err
+	}
+
+	return Device{
+		Id:id,
+		Uuid: uuid,
+		UserId: user_id,
+		IpAddress:ipAddr,
+	}, nil
+}
+
+func (self *MySQLStorage) CreateDevice(uuid, ipAddress string) error{
+	tx ,err := self.Database.Begin()
+
+	if err != nil {
+		log.Printf("Error while opening transaction message: %s", err.Error())
+		return err
+	}
+	defer tx.Rollback()
+
+	_, err = tx.Exec("INSERT device(uuid, ip_addr) VALUES(?, ?)", uuid, ipAddress)
+
+	if err != nil {
+		log.Printf("Error while inserting device: %s", err.Error())
+		return err
+	}
+
+	tx.Commit()
+	return nil
 }
 
 func (m Measurement) String() string {
