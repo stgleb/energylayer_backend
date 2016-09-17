fcf32fa025c91b388dbec8503f2d59d97dd8f747
stgleb <glebstepanov1992@gmail.com>
2016-09-17 19:07:37 +0300

    Fix run method of fixed_pool

--- worker_pool/fixed_worker_pool.go
+++ worker_pool/fixed_worker_pool.go
@@ -1,8 +1,8 @@
 package worker_pool
 
 type WorkerPool struct {
-	Queue           chan chan Job
-	Input           chan Job
+	queue           chan chan Job
+	input           chan Job
 	Stop            chan struct{}
 	maxWorkersCount int
 }
@@ -10,8 +10,8 @@ type WorkerPool struct {
 // NOTE: maximum quantity of gorotines is max(queueSize, maxWorkersCount)
 func NewWorkerPool(queueSize, maxWorkersCount int) {
 	pool := WorkerPool{
-		Queue:           make(chan chan Job, maxWorkersCount),
-		Input:           make(chan Job, queueSize),
+		queue:           make(chan chan Job, maxWorkersCount),
+		input:           make(chan Job, queueSize),
 		Stop:            make(chan struct{}),
 		maxWorkersCount: maxWorkersCount,
 	}
@@ -21,20 +21,24 @@ func NewWorkerPool(queueSize, maxWorkersCount int) {
 }
 
 func (pool WorkerPool) Submit(job Job) {
-	pool.Queue <- job
+	pool.input <- job
+}
+
+func (pool WorkerPool) JobQueue() chan chan Job {
+	return pool.queue
 }
 
 func (pool WorkerPool) run() {
 	for {
 		select {
-		case job := <-pool.Input:
+		case job := <-pool.input:
 			// TODO: remove this goroutine spawn in flavor of
 			// blocking call
 			go func(job Job) {
 				// Obtain input channel of worker
-				jobChan := <-pool.Queue
-				// Submit hob to worker
-				jobChan <- jobChan
+				jobChan := <-pool.queue
+				// Submit job to worker
+				jobChan <- job
 			}(job)
 		case <-pool.Stop:
 			return
