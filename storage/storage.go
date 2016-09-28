57dc25ba79847a64f6c1d37a5f4ab8fadfef4181
stgleb <glebstepanov1992@gmail.com>
2016-09-29 01:34:24 +0300

    Change Storage interface

--- storage/storage.go
+++ storage/storage.go
@@ -2,7 +2,7 @@ package storage
 
 // Storage interface for storing and retrieving data.
 type Storage interface {
-	CreateMeasurement(m *Measurement) error
+	CreateMeasurement(m []Measurement) error
 	GetMeasurement(count int) ([]Measurement, error)
 	GetDeviceById(uuid string) (Device, error)
 	CreateDevice(uuid, ipAddress string, userId int) error
